4bdad6dbd2472ab17c15c928f7744640e7beafce
daKnife997 <evan.jorgensen@gmail.com>
2024-03-10 12:51:05 -0400

    3/10/24 12:50

--- CS Labs/Lab 9.py
+++ CS Labs/Lab 9.py
@@ -7,20 +7,24 @@ numTosses = 1
 
 sum = fairDie_1 + fairDie_2
 
-print(f"{fairDie_1}, {fairDie_2}, ({fairDie_1 + fairDie_2})")
+print(f"{fairDie_1}, {fairDie_2} ({fairDie_1 + fairDie_2})")
+print("Added the rolls together to generate sum")
 
 while sum < 21:
     fairDie_1 = random.randint(1,6)
     fairDie_2 = random.randint(1,6)
     numTosses += 1
-    sumThisRoll = fairDie_1 = fairDie_2
+    sumThisRoll = fairDie_1 + fairDie_2
+    print(f"{fairDie_1}, {fairDie_2} ({sumThisRoll}) ")
     if sumThisRoll % 2 == 0:
         sum = sum + sumThisRoll
+        print("Added to sum, giving", sum)
     elif sumThisRoll < 6:
         sum = sum + 2 * sumThisRoll
+        print("Multiplied by 2 and added to sum, giving", sum)
     else:
         sum = sum - sumThisRoll
-    print(f"{fairDie_1}, {fairDie_2}, ({fairDie_1 + fairDie_2}) ")
+        print("Subtracted from sum, giving", sum)
 
 print(f"The final sum was {sum}")
 print(f"The number of tosses it took was {numTosses}")
