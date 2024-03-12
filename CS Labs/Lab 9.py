67de4e0d28a02989b076e4921c862da857b5638e
daKnife997 <evan.jorgensen@gmail.com>
2024-03-12 09:30:32 -0400

    Added Lab 9 part 3

--- CS Labs/Lab 9.py
+++ CS Labs/Lab 9.py
@@ -1,30 +1,40 @@
 import random as rand
 
-fairDie_1 = rand.randint(1,6)
-fairDie_2 = rand.randint(1,6)
+keepPlaying = True
 
-numTosses = 1
 
-sum = fairDie_1 + fairDie_2
+while keepPlaying:
 
-print(f"{fairDie_1}, {fairDie_2} ({fairDie_1 + fairDie_2})")
-print("Added the rolls together to generate sum")
+    goalSum = int(input("What do you want to be the goal? "))
 
-while sum < 21:
     fairDie_1 = rand.randint(1,6)
     fairDie_2 = rand.randint(1,6)
-    numTosses += 1
-    sumThisRoll = fairDie_1 + fairDie_2
-    print(f"{fairDie_1}, {fairDie_2} ({sumThisRoll}) ")
-    if sumThisRoll % 2 == 0:
-        sum = sum + sumThisRoll
-        print("Added to sum, giving", sum)
-    elif sumThisRoll < 6:
-        sum = sum + 2 * sumThisRoll
-        print("Multiplied by 2 and added to sum, giving", sum)
-    else:
-        sum = sum - sumThisRoll
-        print("Subtracted from sum, giving", sum)
 
-print(f"The final sum was {sum}")
-print(f"The number of tosses it took was {numTosses}")
+    numTosses = 1
+
+    sum = fairDie_1 + fairDie_2
+
+    print(f"{fairDie_1}, {fairDie_2} ({fairDie_1 + fairDie_2})")
+    print("Added the rolls together to generate sum")
+
+    while sum < goalSum:
+        fairDie_1 = rand.randint(1,6)
+        fairDie_2 = rand.randint(1,6)
+        numTosses += 1
+        sumThisRoll = fairDie_1 + fairDie_2
+        print(f"{fairDie_1}, {fairDie_2} ({sumThisRoll}) ")
+        if sumThisRoll % 2 == 0:
+            sum = sum + sumThisRoll
+            print("Added to sum, giving", sum)
+        elif sumThisRoll < 6:
+            sum = sum + 2 * sumThisRoll
+            print("Multiplied by 2 and added to sum, giving", sum)
+        else:
+            sum = sum - sumThisRoll
+            print("Subtracted from sum, giving", sum)
+
+    print(f"The final sum was {sum}")
+    print(f"The number of tosses it took was {numTosses}")
+
+    
+    keepPlaying = input("Want to keep playing? (Y, N)").lower == "y"
