951c1c13059a4917705d1088b6dd61969b9eb55a
daKnife997 <evan.jorgensen@gmail.com>
2024-04-29 10:03:15 -0400

    stuf

--- Python/pygame/Mastermind 3.0.py
+++ Python/pygame/Mastermind 3.0.py
@@ -1,40 +1,100 @@
 import pygame
+import random
 
 pygame.init()
 
 WIDTH = 800
 HEIGHT = 600
+BOARD_X = WIDTH-40
+BOARD_Y = HEIGHT//2
+HINT_BOARD_X = BOARD_X
+HINT_BOARD_Y = HEIGHT // 8
+CENTER = (WIDTH/2, HEIGHT/2)
+
+
 FPS = 60
 
-red = (200, 50, 50)
-blue = (50, 50, 200)
-yellow = (200, 200, 50)
-green = (50, 200, 50)
-black = (10, 10, 10)
-white = (240, 240, 240)
+RED = (200, 50, 50)
+BLUE = (50, 50, 200)
+YELLOW = (200, 200, 50)
+GREEN = (50, 200, 50)
+BLACK = (10, 10, 10)
+WHITE = (240, 240, 240)
 
-bg_color = (100, 100, 200)
-
+BG_COLOR = (50, 50, 100)
+BOARD_COLOR = (40, 40, 40)
+SPOT_COLOR = (75, 75, 75)
 
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 FramePerSec = pygame.time.Clock()
 
 
 
-font = pygame.font.Font("freesansbold.ttf", 32)
 
-text = font.render('yes', True, black)
-textRect = text.get_rect(center = (WIDTH/2, HEIGHT/4))
-textRect.center = (WIDTH/2, HEIGHT/4)
+
+
+def hintBoard():
+    hintBoard.surf = pygame.Surface((HINT_BOARD_X, HINT_BOARD_Y))
+    hintBoard.surf.fill(BOARD_COLOR)
+    hintBoard.rect = hintBoard.surf.get_rect(center =(CENTER[0], CENTER[1] - (HEIGHT // 3)))
+
+inputPinSpotList = []
+hintPinSpotList = []
+hintDividerLineList = []
+
+def Board():
+    Board.surf = pygame.Surface((BOARD_X, BOARD_Y))
+    Board.surf.fill(BOARD_COLOR)
+    Board.rect = Board.surf.get_rect(center = CENTER)
+
+    for x in range (12):
+        for y in range(4):
+            inputPinSpot = pygame.Rect(((x*BOARD_X//12) + BOARD_X//12) - 30, ((y*BOARD_Y//4) + BOARD_X//4) - 15, 30, 30)
+            inputPinSpotList.append(inputPinSpot)
+
+    for x in range (24):
+        for y in range (2):
+            hintPinSpot = pygame.Rect((x * HINT_BOARD_X//24) + BOARD_X//24, (y*HINT_BOARD_Y//2) + HINT_BOARD_Y, 10, 10)
+            hintPinSpotList.append(hintPinSpot)
+    
+    for x in range(11):
+        dividerLine = pygame.Rect(((x*BOARD_X//12) + BOARD_X//12) + 20, CENTER[1]//4-12, 3, HINT_BOARD_Y)
+        hintDividerLineList.append(dividerLine)
+
+def pins():
+    for i in range(len(inputPinSpotList)):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+
+                if inputPinSpotList[i].collidepoint(pygame.mouse.get_pos()) and event.key == pygame.K_1 :
+                    print("yes")
+
+
+
+Board()
+hintBoard()
+
+
+
+
 
 running = True
 while running:
 
-    display.fill(bg_color)
-
-    display.blit(text, textRect)
+    display.fill(BG_COLOR)
 
 
+    display.blit(Board.surf, Board.rect)
+    display.blit(hintBoard.surf, hintBoard.rect)
+                
+    for inputPinSpot in inputPinSpotList:
+        for hintPinSpot in hintPinSpotList:
+            pygame.draw.rect(display, SPOT_COLOR, inputPinSpot)
+            pygame.draw.rect(display, SPOT_COLOR, hintPinSpot)
+    for dividerLine in hintDividerLineList:
+        pygame.draw.rect(display, BLACK, dividerLine) 
+
+    pins()
     pygame.display.update()
     FramePerSec.tick(FPS)
 
