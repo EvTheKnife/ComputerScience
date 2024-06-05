9d3e4e164ed495e2da6602d81eb99d5429ddb875
daKnife <evan.jorgensen@gmail.com>
2024-06-05 09:28:44 -0400

    2024-06-05T13:28:43.954Z:

--- Web Dev (JS)/Final Project/TTT.js
+++ Web Dev (JS)/Final Project/TTT.js
@@ -1,38 +1,100 @@
-function Master_Function(Box_Pos) {
-    var img_ID
-    switch(Box_Pos) {
-        case 'TL':
-            var img_ID = document.getElementById('Box_TL')
-            change_Box(img_ID)
-        case 'TC':
-            var img_ID = document.getElementById('Box_TC')
-            change_Box(img_ID)
-        case 'TR':
-            var img_ID = document.getElementById('Box_TR')
-            change_Box(img_ID)
-        case 'CL':
-            var img_ID = document.getElementById('Box_CL')
-            change_Box(img_ID)
-        case 'CC':
-            var img_ID = document.getElementById('Box_CC')
-            change_Box(img_ID)
-        case 'CR':
-            var img_ID = document.getElementById('Box_CR')
-            change_Box(img_ID)
-        case 'BL':
-            var img_ID = document.getElementById('Box_BL')
-            change_Box(img_ID)
-        case 'BC':
-            var img_ID = document.getElementById('Box_BC')
-            change_Box(img_ID)
-        case 'BR':
-            var img_ID = document.getElementById('Box_BR')
-            change_Box(img_ID)
-    }
+var turn = 0;
 
+function Master_Function(Box_Pos) {
+    switch (turn) {
+        case 0:
+            switch(Box_Pos) {
+            case 'TL':
+                var img_ID = document.getElementById('Box_TL')
+                change_Box_X(img_ID)
+                break;
+            case 'TC':
+                var img_ID = document.getElementById('Box_TC')
+                change_Box_X(img_ID)
+                break;
+            case 'TR':
+                var img_ID = document.getElementById('Box_TR')
+                change_Box_X(img_ID)
+                break;
+            case 'CL':
+                var img_ID = document.getElementById('Box_CL')
+                change_Box_X(img_ID)
+                break;
+            case 'CC':
+                var img_ID = document.getElementById('Box_CC')
+                change_Box_X(img_ID)
+                break;
+            case 'CR':
+                var img_ID = document.getElementById('Box_CR')
+                change_Box_X(img_ID)
+                break;
+            case 'BL':
+                var img_ID = document.getElementById('Box_BL')
+                change_Box_X(img_ID)
+                break;
+            case 'BC':
+                var img_ID = document.getElementById('Box_BC')
+                change_Box_X(img_ID)
+                break;
+            case 'BR':
+                var img_ID = document.getElementById('Box_BR')
+                change_Box_X(img_ID)
+                break;
+            }
+            turn = 1;
+
+            break;
+        case 1:
+            switch(Box_Pos) {
+                case 'TL':
+                    var img_ID = document.getElementById('Box_TL')
+                    change_Box_O(img_ID)
+                    break;
+                case 'TC':
+                    var img_ID = document.getElementById('Box_TC')
+                    change_Box_O(img_ID)
+                    break;
+                case 'TR':
+                    var img_ID = document.getElementById('Box_TR')
+                    change_Box_O(img_ID)
+                    break;
+                case 'CL':
+                    var img_ID = document.getElementById('Box_CL')
+                    change_Box_O(img_ID)
+                    break;
+                case 'CC':
+                    var img_ID = document.getElementById('Box_CC')
+                    change_Box_O(img_ID)
+                    break;
+                case 'CR':
+                    var img_ID = document.getElementById('Box_CR')
+                    change_Box_O(img_ID)
+                    break;
+                case 'BL':
+                    var img_ID = document.getElementById('Box_BL')
+                    change_Box_O(img_ID)
+                    break;
+                case 'BC':
+                    var img_ID = document.getElementById('Box_BC')
+                    change_Box_O(img_ID)
+                    break;
+                case 'BR':
+                    var img_ID = document.getElementById('Box_BR')
+                    change_Box_O(img_ID)
+                    break;
+            }
+            turn = 0;
+            console.log(turn)
+            break;
+    
+    
+    }
 
 }
 
-function change_Box(img_ID) {
+function change_Box_X(img_ID) {
     img_ID.src="TTT_Images/istockphoto-1362671957-100x100.jpg"
+}
+function change_Box_O(img_ID) {
+    img_ID.src="TTT_Images/O_icon.png"
 }
