93a5f9af2f780dc7f17d8ea53c8ad4e76aba872a
daKnife <evan.jorgensen@gmail.com>
2024-06-05 08:54:42 -0400

    2024-06-05T12:54:42.501Z:

--- Web Dev (JS)/Final Project/TTT.js
+++ Web Dev (JS)/Final Project/TTT.js
@@ -1,1 +1,38 @@
-function direct_B
+function Master_Function(Box_Pos) {
+    var img_ID
+    switch(Box_Pos) {
+        case 'TL':
+            var img_ID = document.getElementById('Box_TL')
+            change_Box(img_ID)
+        case 'TC':
+            var img_ID = document.getElementById('Box_TC')
+            change_Box(img_ID)
+        case 'TR':
+            var img_ID = document.getElementById('Box_TR')
+            change_Box(img_ID)
+        case 'CL':
+            var img_ID = document.getElementById('Box_CL')
+            change_Box(img_ID)
+        case 'CC':
+            var img_ID = document.getElementById('Box_CC')
+            change_Box(img_ID)
+        case 'CR':
+            var img_ID = document.getElementById('Box_CR')
+            change_Box(img_ID)
+        case 'BL':
+            var img_ID = document.getElementById('Box_BL')
+            change_Box(img_ID)
+        case 'BC':
+            var img_ID = document.getElementById('Box_BC')
+            change_Box(img_ID)
+        case 'BR':
+            var img_ID = document.getElementById('Box_BR')
+            change_Box(img_ID)
+    }
+
+
+}
+
+function change_Box(img_ID) {
+    document.getElementById(img_ID).src="TTT_Images/istockphoto-1362671957-100x100.jpg"
+}
