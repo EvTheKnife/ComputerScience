91ea6c7d498a891f444a7ca8368a565ef50617cf
daKnife <evan.jorgensen@gmail.com>
2024-06-11 10:29:19 -0400

    2024-06-11T14:29:19.547Z:

--- Web Dev (JS)/Final Project/DNB.js
+++ Web Dev (JS)/Final Project/DNB.js
@@ -1,1 +1,6 @@
-var canvas = document.getElementById("Canvas").getContext
+function draw() {
+    var canvas = document.getElementById('Canvas').getContext("2d");
+    canvas.
+    canvas.font = "30px Arial";
+    canvas.fillText("Hello World", 10, 50);
+}
