ecc5b3d69c1ec085d7c9f9dc48f90f667ec803bc
daKnife <evan.jorgensen@gmail.com>
2024-06-13 09:31:21 -0400

    2024-06-13T13:31:21.300Z:

--- Web Dev (JS)/Final Project/HM.js
+++ Web Dev (JS)/Final Project/HM.js
@@ -1,13 +1,50 @@
-window.onload(masterFunction())
+//window.onload(drawFunction())
+//window.onload(HM_logic())
 
-function masterFunction() {
-    //drawRect(100, 475, 475, 100, '#000000', false, 1.0)
+function HM_logic() {
+    var real_answer_key = "why"
+    var answer_key = document.getElementById("answer").innerHTML
+    var user_input = document.getElementById("user_input")
+    user_input = user_input.value
+    answer_key = answer_key.replace(/\s+/g, '');
+    answer_key = answer_key.replace('|', '');
+
+    while (real_answer_key.includes(user_input)) {
+        var input_index = real_answer_key.indexOf(user_input)
+        if (input_index != -1) {
+            answer_key = answer_key.replace([input_index], user_input);
+            real_answer_key = real_answer_key.replace([input_index], "_")
+        }   
+    }
+
+    //console.log(answer_key)
+}
+
+
+
+
+function drawFunction() {
+
+    //Draw Base
     drawLine(100, 475, 475, 475)
     drawLine(510, 475, 525, 475)
-    drawLine(525, 475, 525, 575, )
+    drawLine(525, 475, 525, 575)
+    drawLine(525, 575, 100, 575)
+    drawLine(100, 575, 100, 475)
 
-    drawLine(510, 475, 510, 125, "#000000")
-    drawLine(475, 475, 475, 125, "#000000")
+
+    //Draw rest
+    drawLine(510, 475, 510, 40)
+    drawLine(475, 475, 475, 75)
+    drawLine(475, 125, 425, 75)
+    drawLine(475, 150, 400, 75)
+    drawLine(475, 75, 275, 75)
+    drawLine(510, 40, 235, 40)
+    drawLine(235, 40, 235, 150)
+    drawLine(275, 75, 275, 150)
+    drawLine(235, 150, 275, 150)
+    drawLine(255, 150, 255, 200)
+
 
 
 }
